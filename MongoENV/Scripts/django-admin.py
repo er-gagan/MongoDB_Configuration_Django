c1c8627d2d6f56eb539a977c74edbc9320100774
Gagan Aggarwal <62302397+pythoniseasy-hub@users.noreply.github.com>
2020-12-07 10:27:15 +0530

    Virtal Env Create, Project Create, install modules
    Virtal Env Create, Project Create, install modules

---
+++ MongoENV/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\gagan\OneDrive\Documents\GitHub\MongoDB_Configuration_Django\MongoENV\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
